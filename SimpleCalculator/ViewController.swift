1f3391b70836efcb0a2451ef1e242885d90c23b6
Omer Apaydin <omerapaydin90@gmail.com>
2024-04-27 19:07:43 +0300

    First

--- SimpleCalculator/ViewController.swift
+++ SimpleCalculator/ViewController.swift
@@ -8,12 +8,73 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var numberOne: UITextField!
+    
+    @IBOutlet weak var numberTwo: UITextField!
+    
+    @IBOutlet weak var resultText: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
 
+    @IBAction func top(_ sender: Any) {
+        
+        if let sayi1 = Int(numberOne.text!){
+            if let sayi2 = Int(numberTwo.text!){
+                
+                var sonuc = sayi1 + sayi2
+                
+                resultText.text = String(sonuc)
+            }
+        }
 
+        
+        
+        
+    }
+    
+    @IBAction func cik(_ sender: Any) {
+    
+        if let sayi1 = Int(numberOne.text!){
+            if let sayi2 = Int(numberTwo.text!){
+                
+                var sonuc = sayi1 - sayi2
+                
+                resultText.text = String(sonuc)
+            }
+        }
+
+        
+    }
+    
+    @IBAction func carp(_ sender: Any) {
+        if let sayi1 = Int(numberOne.text!){
+            if let sayi2 = Int(numberTwo.text!){
+                
+                var sonuc = sayi1 * sayi2
+                
+                resultText.text = String(sonuc)
+            }
+        }
+
+        
+    }
+    
+    @IBAction func bol(_ sender: Any) {
+        if let sayi1 = Int(numberOne.text!){
+            if let sayi2 = Int(numberTwo.text!){
+                
+                var sonuc = sayi1 / sayi2
+                
+                resultText.text = String(sonuc)
+            }
+        }
+
+        
+    }
+    
 }
 
